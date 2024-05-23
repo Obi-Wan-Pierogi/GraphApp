2c512c91624a3b52988141ab568965f1d41f9901
Jesse Harlan <jesse.harlan@centralia.edu>
2024-05-23 11:51:49 -0700

    added printting of the adjacency matrix

--- GraphLib/DirectedGraph.cs
+++ GraphLib/DirectedGraph.cs
@@ -1,4 +1,6 @@
-﻿namespace GraphLib
+﻿using System.Runtime.ExceptionServices;
+
+namespace GraphLib
 {
     public class DirectedGraph
     {
@@ -12,6 +14,62 @@
             Vertices.Add(v);
 
             return v;
+        }
+
+        public int?[,] CreateAdjMatrix()
+        {
+            // make a 2d array to represent all verticies
+            int?[,] AdjMatrix = new int?[Vertices.Count,Vertices.Count];
+
+            for (int i = 0; i < Vertices.Count; i++)
+            {
+                Vertex v1 = Vertices[i];
+
+                for (int j = 0; j < Vertices.Count; j++)
+                {
+                    Vertex v2 = Vertices[j];
+
+                    Edge edge = v1.Edges.FirstOrDefault(e => e.Child == v2);
+
+                    if (edge != null)
+                    {
+                        AdjMatrix[i, j] = edge.Weight;
+                    }
+                }
+            }
+
+            return AdjMatrix;
+        }
+
+        public void PrintMatrix()
+        {
+            var matrix = CreateAdjMatrix();
+
+            Console.Write("\t");
+            for (int i = 0; i < Vertices.Count; i++)
+            {
+                Console.Write($" {Vertices[i].Label} ");
+            }
+            Console.WriteLine();
+
+
+            for (int i = 0; i < Vertices.Count; i++)
+            {
+                Console.Write($"{Vertices[i].Label}\t");
+
+                for (int j = 0; j < Vertices.Count; j++)
+                {
+                    if (matrix[i, j] != null)
+                    {
+                        Console.Write($"[{matrix[i, j].ToString()}]");
+                    }
+                    else
+                    {
+                        Console.Write("[.]");
+                    }
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
