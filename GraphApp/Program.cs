b1d9409104b0680be158650c180b9caf3465a6dc
Jesse Harlan <gotsanity@gmail.com>
2024-05-28 21:04:47 -0700

    Added Lazy Dijkstras

--- GraphApp/Program.cs
+++ GraphApp/Program.cs
@@ -7,9 +7,18 @@ DirectedGraph graph = new DirectedGraph();
 var a = graph.AddVertex("A");
 var b = graph.AddVertex("B");
 var c = graph.AddVertex("C");
+var d = graph.AddVertex("D");
+var e = graph.AddVertex("E");
 
 a.AddEdge(b, 5).AddEdge(c, 2);
 b.AddEdge(c, 1);
+b.AddEdge(d, 2);
+c.AddEdge(d, 1).AddEdge(e, 1);
+
+
 
 graph.PrintMatrix();
 
+var adj = graph.CreateAdjMatrix();
+
+graph.Dijkstra(adj, 4);
--- GraphLib/DirectedGraph.cs
+++ GraphLib/DirectedGraph.cs
@@ -16,10 +16,10 @@ namespace GraphLib
             return v;
         }
 
-        public int?[,] CreateAdjMatrix()
+        public int[,] CreateAdjMatrix()
         {
             // make a 2d array to represent all verticies
-            int?[,] AdjMatrix = new int?[Vertices.Count,Vertices.Count];
+            int[,] AdjMatrix = new int[Vertices.Count,Vertices.Count];
 
             for (int i = 0; i < Vertices.Count; i++)
             {
@@ -40,6 +40,77 @@ namespace GraphLib
 
             return AdjMatrix;
         }
+
+        public int MinDistance(int[] dist, bool[] visits)
+        {
+            int min = int.MaxValue;
+            int min_index = -1;
+
+            for (int i = 0; i < Vertices.Count; i++)
+            {
+                if (visits[i] == false && dist[i] <= min)
+                {
+                    min = dist[i];
+                    min_index = i;
+                }
+            }
+
+            return min_index;
+        }
+
+        public void PrintSolution(int[] dist, int n)
+        {
+            Console.Write("Vertex     Distance from Source\n");
+
+            for (int i = 0; i < Vertices.Count; i++)
+            {
+                Console.Write(Vertices[i].Label + " \t\t " + dist[i] + "\n");
+            }
+        }
+
+        public void Dijkstra(int[,] graph, int src)
+        {
+            int[] dist = new int[Vertices.Count];
+            bool[] visits = new bool[Vertices.Count];
+
+            for (int i = 0; i < Vertices.Count; i++)
+            {
+                dist[i] = int.MaxValue;
+                visits[i] = false;
+            }
+
+            dist[src] = 0;
+
+            for (int count = 0; count < Vertices.Count - 1; count++)
+            {
+                // pick min distance vertex from the set of vertices not yet processed
+                int u = MinDistance(dist, visits);
+
+                // mark the picked vertex as visited
+                visits[u] = true;
+
+                // update dist value of the adjacent vertices of the picked vertex
+                for (int v = 0; v < Vertices.Count; v++)
+                {
+                    /* update dist[v] only if:
+                        it is not visited,
+                        there is an edge for u to v,
+                        and the total weight of path from src to v through u is smaller than the current value of dist[v]
+                    */
+
+                    if (!visits[v] &&
+                        graph[u, v] != 0 &&
+                        dist[u] != int.MaxValue &&
+                        dist[u] + graph[u, v] < dist[v])
+                    {
+                        dist[v] = dist[u] + graph[u, v];
+                    }
+                }
+            }
+
+            PrintSolution(dist, Vertices.Count);
+        }
+        
 
         public void PrintMatrix()
         {
